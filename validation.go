bb7d393a00ef0564756fc1e1f56bd65580a3eee5
Takashi Yokoyama <tksyokoyama@gmail.com>
2012-05-27 13:20:23 +0900

    未入力チェックと整数型チェックを実装。 整数型チェックは未入力時にエラーにならないテストまで完了。

--- validation.go
+++ validation.go
@@ -1,21 +1,41 @@
 package validation
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type Validators struct {
-	errorMap map[string] string		// 検証エラーメッセージ
+	errorMap map[string] []string	// 検証エラーメッセージ
 	rules []Validator				// 検証ルール
 }
 
 // 検証インターフェイス
 type Validator interface {
+	Create(string) Validator
 	Validate(r *http.Request) bool
+	DefaultMessage() string
+	Message() string				// メッセージ取得
+	ParameterName() string			// パラメータ取得
 }
 
 type RequiredValidator struct {
 	ParamName string			// パラメータ名
+	ErrorMessage string			// メッセージ
+}
+
+type IntValidator struct {
+	ParamName string			// パラメータ名
+	BitSize int					// サイズ
+	ErrorMessage string			// メッセージ
+}
+
+// 未入力検証インスタンス生成
+func (v RequiredValidator) Create(paramName string) Validator {
+	v.ParamName  = paramName
+
+	return v
 }
 
 // 未入力検証
@@ -31,21 +51,94 @@ func (v RequiredValidator) Validate(r *http.Request) bool {
 	return result
 }
 
+// 未入力時のデフォルトエラーメッセージ
+func(v RequiredValidator) DefaultMessage() string {
+	return fmt.Sprintf("%sは必須です", v.ParamName)
+}
+
+func(v RequiredValidator) ParameterName() string {
+	return v.ParamName
+}
+
+func(v RequiredValidator) Message() string {
+	return v.ErrorMessage
+}
+
+// 整数型検証インスタンス生成
+func (v IntValidator) Create(paramName string) Validator {
+	v.ParamName  = paramName
+
+	return v
+}
+
+// 整数型検証
+func (v IntValidator) Validate(r *http.Request) bool {
+	var result bool
+	param := r.FormValue(v.ParamName)
+	if param == "" {
+		// 未入力はOKとする。
+		result = true
+	} else {
+		// 取り敢えず型変換を試みる。
+		if _, err := strconv.ParseInt(param, 10, v.BitSize); err != nil {
+			result = false
+		} else {
+			result = true			
+		}
+	}
+
+	return result	
+}
+
+// 整数型検証失敗時のデフォルトエラーメッセージ
+func(v IntValidator) DefaultMessage() string {
+	return fmt.Sprintf("%sは%dビット整数ではありません。", v.ParamName, v.BitSize)
+}
+
+func(v IntValidator) Message() string {
+	return v.ErrorMessage
+}
+
+func(v IntValidator) ParameterName() string {
+	return v.ParamName
+}
+
 // 検証ルールの追加
 func (v *Validators) Add(paramName string, rule ...Validator) {
 	for _, r := range rule {
-		v.rules = append(v.rules, r)
+		v.rules = append(v.rules, r.Create(paramName))
 	}
 }
 
 // 検証
 func (v *Validators) Validate(r *http.Request) bool {
-	for _, validator := range v.rules {
-		if result := validator.Validate(r); result == false {
+	var result bool = true
+	for pos, validator := range v.rules {
+		if validate := validator.Validate(r); validate == false {
 			// 検証失敗
+			message := v.rules[pos].Message()
+			if message == "" {
+				message = v.rules[pos].DefaultMessage()
+			}
 
+			if v.errorMap == nil {
+				v.errorMap = make(map[string] []string)
+			}
+			fmt.Printf("DEBUG: %s", v.rules[pos].ParameterName())
+			v.errorMap[v.rules[pos].ParameterName()] = append(v.errorMap[v.rules[pos].ParameterName()], message)
+			result = false
 		}
 	}
 
-	return true
+	return result
+}
+
+// 検証エラー取得
+func (v *Validators) Errors() map[string] []string {
+	return v.errorMap
+}
+
+// パラメータ毎の検証エラー取得
+func (v *Validators) Error(paramName string) []string {
+	return v.errorMap[paramName]
 }
--- validation_test.go
+++ validation_test.go
@@ -1,11 +1,16 @@
 package validation
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	var validators Validators
 
 	var required RequiredValidator
+	var integer IntValidator
+
 	validators.Add("param1", required)
 
 	// 検証ルールを１個追加
@@ -13,5 +18,59 @@ func TestAdd(t *testing.T) {
 		t.Errorf("validators.Add(\"param1\", required)")
 	}
 
-	
+	// 別のパラメータで検証ルールを２個追加
+	validators.Add("param2", required, integer)
+
+	if len(validators.rules) != 3 {
+		t.Errorf("validators.Add(\"param2\", required, int32type)")		
+	}
+}
+
+// リクエストパラメータなし（必須チェック）
+func TestValidateNoParam(t *testing.T) {
+	r, err := http.NewRequest("Get", "http://localhost", nil)
+	if err != nil {
+		t.Errorf("Request Create Error")
+	}
+
+	var validators Validators
+
+	var param1Required RequiredValidator
+
+	validators.Add("param1", param1Required)
+
+	if result := validators.Validate(r); result == true {
+		t.Errorf("validators.Validate(r) is failed")
+	}
+
+	if len(validators.Error("param1")) != 1 {
+		t.Errorf("validators errorMap is wrong. [%d != %d]", len(validators.Error("param1")), 1)
+	}
+
+	if validators.Error("param1")[0] != "param1は必須です" {
+		t.Errorf("validators errorMap is wrong. [%s != %s]", validators.Error("param1")[0], "param1は必須です")
+	}
+}
+
+// リクエストパラメータなし（整数型、必須ではない）
+func TestValidateNoParamInteger(t *testing.T) {
+	r, err := http.NewRequest("Get", "http://localhost", nil)
+	if err != nil {
+		t.Errorf("Request Create Error")
+	}
+
+	var validators Validators
+
+	var param1Integer IntValidator
+	param1Integer.BitSize = 32
+
+	validators.Add("param1", param1Integer)
+
+	if result := validators.Validate(r); result == false {
+		t.Errorf("validators.Validate(r) is failed")
+	}
+
+	if len(validators.Error("param1")) != 0 {
+		t.Errorf("validators errorMap is wrong.")
+	}
 }
